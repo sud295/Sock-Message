de9cdbcc2bba18d1e1e9c02acc15d64cbfbc0e0c
sud295 <sonicsid12@gmail.com>
2023-07-23 18:26:50 -0400

    Re-designed port maintainance and re-connections

--- peer.py
+++ peer.py
@@ -3,21 +3,35 @@ import select
 import threading
 import time
 import copy
+import os
 from candidate import Candidate
 
 def leader_thread(event, net_part: list) -> None:
+    '''
+    This function outlies the unique tasks of a leader peer
+    The leader sends heartbeats at regular intervals of 1.5 seconds
+    These heartbeat messages contain the adresses of the receiving ends of all network participants
+    The leader uses its own port for communicating with the followers so as to not get mixed with the other messages
+    '''
+
+    # When a new leader is elected, it will broadcast its adress for the followers to note
     broadcast_addr = False
+
+    '''
+    Since the innitial election process and the re-election processes are different, there is a distinction between 
+    being a leader and being a new leader
+    '''
     global new_leader
     global network_participants
     global HOST
     global PORT
-    network_participants = copy.deepcopy(net_part)
-    if new_leader:
-        prev_participants = []
-    else:
-        prev_participants = copy.deepcopy(network_participants)
-    participant_sockets = []
 
+    network_participants = copy.deepcopy(net_part)
+
+    global participant_dict
+    participant_dict = {}
+
+    # As long as the user does not type $exit
     while not event.is_set():
         time.sleep(1.5)
         message = "$HEARTBEAT$"
@@ -25,71 +39,56 @@ def leader_thread(event, net_part: list) -> None:
             message += participant
             message += ","
         message = message[:-1]
-        if prev_participants != network_participants:
-            new_participants = set(network_participants) - set(prev_participants)
-            closed_sockets = []
 
-            for participant in new_participants:
+        for participant in network_participants:
+            sock = participant_dict.get(participant)
+            if sock == None:
                 participant_IP, participant_port = participant.split(":")
                 participant_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 try:
                     participant_socket.connect((participant_IP, int(participant_port)))
-                    participant_sockets.append(participant_socket)
                 except:
                     print(f"Could not add new participant {participant_IP}:{participant_port}")
-                    network_participants.remove(participant)  # Remove participant from the array
+                    network_participants.remove(participant)  
                     continue
+                participant_dict[participant] = participant_socket
+                sock = participant_dict.get(participant)
+            try:
+                sock.sendall(message.encode())
+            except:
+                network_participants.remove(participant)
+                participant_dict[participant] = None
 
-            for participant_socket in participant_sockets:
-                try:
-                    participant_socket.sendall(message.encode())
-                except:
-                    closed_sockets.append(participant_socket)
-                    continue
-
-            for closed_socket in closed_sockets:
-                participant_sockets.remove(closed_socket)
-                closed_socket.close()
-
-            prev_participants = copy.deepcopy(network_participants)
-        else:
-            for participant_socket in participant_sockets:
-                try:
-                    participant_socket.sendall(message.encode())
-                except:
-                    participant_sockets.remove(participant_socket)
-                    # Handle disconnection and removal of the participant from the array
-                    for participant in network_participants:
-                        participant_IP, participant_port = participant.split(":")
-                        if participant_socket.getpeername() == (participant_IP, int(participant_port)):
-                            network_participants.remove(participant)
-                            break
-                    participant_socket.close()
-                    continue
         if not broadcast_addr and new_leader:
             message = f"$FORCE leader${HOST}:{PORT}".encode()
-            for participant_socket in participant_sockets:
+            for participant in network_participants:
+                sock = participant_dict.get(participant)
+                if sock == None:
+                    participant_IP, participant_port = participant.split(":")
+                    participant_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    try:
+                        participant_socket.connect((participant_IP, int(participant_port)))
+                    except:
+                        print(f"Could not add new participant {participant_IP}:{participant_port}")
+                        network_participants.remove(participant)  
+                        continue
+                    participant_dict[participant] = participant_socket
+                    sock = participant_dict.get(participant)
                 try:
-                    participant_socket.sendall(message.encode())
+                    sock.sendall(message.encode())
                 except:
-                    participant_sockets.remove(participant_socket)
-                    # Handle disconnection and removal of the participant from the array
-                    for participant in network_participants:
-                        participant_IP, participant_port = participant.split(":")
-                        if participant_socket.getpeername() == (participant_IP, int(participant_port)):
-                            network_participants.remove(participant)
-                            break
-                    participant_socket.close()
-                    continue
+                    network_participants.remove(participant)
+                    participant_dict[participant] = None
             broadcast_addr = True
+
+        print(message)
 
 def send_thread(event) -> None:
     global network_participants
     network_participants = []
-    prev_participants = copy.deepcopy(network_participants)
-    participant_sockets = []
     global leader_socket
-    
+    global participant_dict
+
     while not event.is_set():
         message = input("")
 
@@ -97,46 +96,30 @@ def send_thread(event) -> None:
             event.set()
             break
         
-        try:
-            if not leader:
+        if not leader:
+            try:
                 leader_socket.sendall(message.encode())
-        except:
-            pass
+            except:
+                pass
             
-        if prev_participants != network_participants:
-            new_participants = set(network_participants) - set(prev_participants)
-            closed_sockets = []
-
-            for participant in new_participants:
+        for participant in network_participants:
+            sock = participant_dict.get(participant)
+            if sock == None:
                 participant_IP, participant_port = participant.split(":")
                 participant_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 try:
                     participant_socket.connect((participant_IP, int(participant_port)))
-                    participant_sockets.append(participant_socket)
                 except:
                     print(f"Could not add new participant {participant_IP}:{participant_port}")
+                    network_participants.remove(participant)  
                     continue
-
-            for participant_socket in participant_sockets:
-                try:
-                    participant_socket.sendall(message.encode())
-                except:
-                    closed_sockets.append(participant_socket)
-                    continue
-            
-            for closed_socket in closed_sockets:
-                participant_sockets.remove(closed_socket)
-                closed_socket.close()
-
-            prev_participants = copy.deepcopy(network_participants)
-        else:
-            for participant_socket in participant_sockets:
-                try:
-                    participant_socket.sendall(message.encode())
-                except:
-                    participant_sockets.remove(participant_socket)
-                    participant_socket.close()
-                    continue
+                participant_dict[participant] = participant_socket
+                sock = participant_dict.get(participant)
+            try:
+                sock.sendall(message.encode())
+            except:
+                network_participants.remove(participant)
+                participant_dict[participant] = None
 
 def timer_thread(event) -> None:
     global election_ongoing
